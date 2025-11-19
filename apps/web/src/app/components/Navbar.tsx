18ceea83fd91ec2525975d4dadbe4bf58d7db43f
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2025-11-19 19:41:15 +0000

    containerised the bars and navbar into components

---
+++ apps/web/src/app/components/Navbar.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+export default function Navbar() {
+    return (
+        <nav className="absolute flex h-[63px] px-4 gap-4 mt-8 pl-15 w-full justify-between ">
+            {/* navbar title and pic */}
+            <div className="flex items-center">
+                <span className="flex mr-2 -translate-y-1 text-3xl ml-2">
+                    aetherwave
+                </span>
+                <div className="ml-6 mr-6 opacity-50">|</div>
+                <ul className="flex flex-row gap-6 ml-2 text-lg">
+                    <li>
+                        <a href="">home</a>
+                    </li>
+                    <li className="opacity-50">|</li>
+                    <li>
+                        <a href="">portfolio</a>
+                    </li>
+                    <li className="opacity-50">|</li>
+                    <li>
+                        <a href="">bruh</a>
+                    </li>
+                    <li className="opacity-50">|</li>
+                    <li>
+                        <a href="">list</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="items-center flex mr-10">
+                <a href="">Login</a>
+            </div>
+        </nav>
+    );
+}
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, Michroma } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import Bars from "./components/Bars";
 
 const inter = Inter({
     subsets: ["latin"],
@@ -30,48 +32,9 @@ export default function RootLayout({
             <body
                 className={`${michroma.variable} ${inter.variable} antialiased`}
             >
+                <Bars />
                 {/* { NAVBAR } */}
-                <div className="relative flex h-0 overflow-visible w-full top-0 left-0">
-                    <div className="absolute h-2 w-full bg-(--bar-red) barIndexRed top-0 overflow-visible"></div>
-                    <div className="absolute h-2 w-full bg-(--bar-purple) barIndexPurple top-2 overflow-visible"></div>
-                    <div className="absolute h-2 w-full bg-(--bar-teal) barIndexTeal top-4 overflow-visible"></div>
-                    <div className="absolute h-2 w-full bg-(--bar-amber) barIndexAmber top-6 overflow-visible"></div>
-                    <div className="absolute h-8 w-full bg-(--bg) z-50 rotatedNavBar -top-7"></div>
-                    <div className="absolute h-4 w-full bg-(--bar-red) translate-1 rotatedNavBar barIndexRed overflow-visible top-0"></div>
-                    <div className="absolute h-4 w-full bg-(--bar-purple) translate-2 rotatedNavBar barIndexPurple overflow-visible top-1"></div>
-                    <div className="absolute h-4 w-full bg-(--bar-teal) translate-3 rotatedNavBar barIndexTeal overflow-visible top-2"></div>
-                    <div className="absolute h-4 w-full bg-(--bar-amber) translate-4 rotatedNavBar barIndexAmber overflow-visible top-3 "></div>
-
-                    <nav className="absolute flex h-[63px] px-4 gap-4 mt-8 pl-15 w-full justify-between ">
-                        {/* navbar title and pic */}
-                        <div className="flex items-center flex-2">
-                            <span className="flex mr-2 -translate-y-1 text-3xl">
-                                aetherwave
-                            </span>
-                            <div className="ml-6 mr-6">|</div>
-                            <ul className="flex flex-row gap-6 ml-2 text-lg">
-                                <li>
-                                    <a href="">home</a>
-                                </li>
-                                <div className="ml-6 mr-6">|</div>
-                                <li>
-                                    <a href="">portfolio</a>
-                                </li>
-                                <div className="ml-6 mr-6">|</div>
-                                <li>
-                                    <a href="">bruh</a>
-                                </li>
-                                <div className="ml-6 mr-6">|</div>
-                                <li>
-                                    <a href="">list</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="flex-end items-center flex mr-10">
-                            <a href="">Login</a>
-                        </div>
-                    </nav>
-                </div>
+                <Navbar />
                 {children}
             </body>
         </html>
