55bfe4fdb7cceb07eea5887e5ba102c56955b909
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2025-11-19 14:09:44 +0000

    add: brand colours

---
+++ packages/config/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                // Brand Colors
+                // Primary
+                offBlack: "#0D0C0A",
+                offTan: "#F3EDE2",
+                // Accent
+                barRed: "#E25B6A",
+                barAmber: "#F2B544",
+                barTeal: "#38BCA3",
+                barPuple: "#7C43D1",
+            }
+        }
+    }
+};
