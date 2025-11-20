aac6f9338a18e463ec42754399b86d9448d52968
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2025-11-20 15:09:27 +0000

    add: register models in admin

--- apps/api/backend/projects/admin.py
+++ apps/api/backend/projects/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Tag, Project
 
 # Register your models here.
+admin.site.site_header = "AetherPort Projects Admin"
+admin.site.site_title = "AetherPort Projects Admin Portal"
+admin.site.index_title = "Welcome to AetherPort Projects Admin"
+admin.site.empty_value_display = "- Data field is empty -"
+
+@admin.register(Tag)
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'featured', 'order', 'created_at')
+    search_fields = ('title', 'tagline',)
+    list_filter = ('featured', 'completed',)
+    ordering = ('order', '-created_at')
+    filter_horizontal = ('tags',)
