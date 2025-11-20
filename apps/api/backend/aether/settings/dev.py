3a7d251d4df7cd8e06525f06360fa082eb06b1be
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2025-11-20 13:07:00 +0000

    add: django backend setup

---
+++ apps/api/backend/aether/settings/dev.py
@@ -0,0 +1,42 @@
+from .base import *
+from urllib.parse import urlparse, parse_qs
+
+DEBUG = True
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "your-default-secret-key")
+
+ALLOWED_HOSTS = ["*"]
+
+DATABASE_URL = os.environ.get("DEV_DATABASE_URL")
+stripURL = DATABASE_URL.split('?')[0]
+parsed = urlparse(stripURL)
+
+if DATABASE_URL:
+    print("Using DEV database")
+    DATABASES = {
+        'default': {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": parsed.path.lstrip('/'),
+            "USER": parsed.username,
+            "PASSWORD": parsed.password,
+            "HOST": parsed.hostname,
+            "PORT": parsed.port or "5432",
+            "OPTIONS": {
+                "sslmode": "require",
+            },
+        }
+    }
+
+else:
+    print("Using fallback database, DEV url could not be parsed.")
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+    
+    
---
+++ apps/api/backend/aether/settings/prod.py
@@ -0,0 +1,39 @@
+from .base import * 
+from urllib.parse import urlparse
+
+DEBUG = False
+
+FLY = os.environ.get("FLYIO_HOST")
+
+FLYURL = f"{FLY}.fly.dev" if FLY else None
+
+ALLOWED_HOSTS = [
+    FLYURL,
+    "localhost",
+    ".fly.dev",
+]
+
+ALLOWED_HOSTS = [host for host in ALLOWED_HOSTS if host] # Remove any None values so no type errors occur
+
+print("Using PROD database")
+DATABASE_URL = os.environ.get("PROD_DATABASE_URL")
+if DATABASE_URL:
+    parsed = urlparse(DATABASE_URL)
+    DATABASES = {
+        'default': {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": parsed.path.lstrip('/'),
+            "USER": parsed.username,
+            "PASSWORD": parsed.password,
+            "HOST": parsed.hostname,
+            "PORT": parsed.port or "5432",
+            "OPTIONS": {
+                "sslmode": "require",
+            },
+        }
+    }
+
+if FLY:
+    CSRF_TRUSTED_ORIGINS = [f"https://{FLYURL}"]
+else:
+    CSRF_TRUSTED_ORIGINS = []
