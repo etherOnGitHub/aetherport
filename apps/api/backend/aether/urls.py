9e8138c47a942934cb810840d896dd3ac173c253
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2025-12-04 17:50:50 +0000

    add: redirect to django root to api root

--- apps/api/backend/aether/urls.py
+++ apps/api/backend/aether/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('accounts.urls')),
     path('api/', include("aether.api.urls")),
+    path("", lambda r: redirect("/api/v1")),
+
 ]
