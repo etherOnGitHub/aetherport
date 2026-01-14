e76f63a1d3a783b3598bde1bdcf917c66bcbaeb2
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2026-01-14 06:09:54 +0000

    remove non server language

--- apps/web/src/app/projects/page.tsx
+++ apps/web/src/app/projects/page.tsx
@@ -3,8 +3,6 @@
 import Projects from "../components/content/Projects";
 import { getProjects } from "../../../lib/server-api/getProjects";
 
-export const dynamic = "force-dynamic";
-
 export default async function ProjectsPage() {
     const projects = await getProjects();
 
