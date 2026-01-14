603bdccf67d3299c747d343b49e645b8af5295bb
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2026-01-14 06:34:05 +0000

    fix url bug

--- apps/web/lib/server-api/getProjects.ts
+++ apps/web/lib/server-api/getProjects.ts
@@ -4,9 +4,9 @@ import { fetchAPI } from "./serverFetch";
 import type { Project } from "../types/project";
 
 export async function getProjects(): Promise<Project[]> {
-    return fetchAPI("/projects/");
+    return fetchAPI("projects/");
 }
 
 export async function getProjectBySlug(slug: string): Promise<Project> {
-    return fetchAPI(`/projects/${slug}/`);
+    return fetchAPI(`projects/${slug}/`);
 }
